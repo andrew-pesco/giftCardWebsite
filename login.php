3a4d5a3913f3978e64715dcac0ff1f303848b96f
andrew-pesco <andrew.pesco@gmail.com>
2012-08-29 15:56:55 -0400

    session variables

--- login.php
+++ login.php
@@ -4,6 +4,6 @@
 		if(isValidUser($_POST['email_address'],$_POST['password'])==true){
 			setcookie("user",$_POST['email_address'],time()+3600);
 			setcookie("password",$_POST['password'],time()+3600);
-			include myGiftCards.php;
+			include 'myGiftCards.php';
 		}
 	?>
