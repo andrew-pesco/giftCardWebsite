497dc06d80c3c2576f4d930ba96ccc85603ae745
andrew-pesco <andrew.pesco@gmail.com>
2012-08-29 14:17:08 -0400

    change session variable

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 		include 'buildPages.php';
 		if(isValidUser($_POST['email_address'],$_POST['password'])==true){
 			$_SESSION['loggedin']=true;
-			$_SESSION['username']=$_POST['email_address'];s
+			$_SESSION['username']=$_POST['email_address'];
 			buildMyGiftCards();
 		}
 	?>
