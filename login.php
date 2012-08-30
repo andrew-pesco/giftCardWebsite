4e7d4aa00d021c3279a52ad486bbf3b8c42ebb1e
andrew-pesco <andrew.pesco@gmail.com>
2012-08-30 14:34:37 -0400

    added paramaters to newuser

--- login.php
+++ login.php
@@ -2,7 +2,8 @@
 		include 'databasefunctions.php';
 		include 'buildPages.php';
 		if(isValidUser($_POST['email_address'],$_POST['password'])==true){
-			$college=$getCollege($_POST['email_address']);
+			Print "hello";
+			$college=$getCollege(($_POST['email_address']);
 			setcookie("user",$_POST['email_address'],time()+3600);
 			setcookie("password",$_POST['password'],time()+3600);
 			setcookie("college",$college['college'],time()+3600);
