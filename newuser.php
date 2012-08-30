598daf355a9fefdfe893f4d9f2806792f88794e7
andrew-pesco <andrew.pesco@gmail.com>
2012-08-29 20:31:44 -0400

    added pictures

--- newuser.php
+++ newuser.php
@@ -1,4 +1,8 @@
 <?php
 	include 'databasefunctions.php';
 	addUser($_POST['email_address'],$_POST['password'], $_POST['confirm_password']);
+	setcookie("user",$_POST['email_address'],time()+3600);
+	setcookie("password",$_POST['password'],time()+3600);
+	include 'buildPages.php';
+	buildMyGiftCards();
 ?>
