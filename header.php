e733bd4fcb53544ee91b2c1793118319eb432b05
andrew-pesco <andrew.pesco@gmail.com>
2012-08-31 00:28:09 -0400

    added logout

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 			<ul class="nav">
 				<li class="active"><a href="myGiftCards.php">My Gift Cards</a></li>
 				<li><a href="giftCardStore.php">Gift Card Store</a><li>
+				<li><a href="logout.php">Sign Out</a></li>
 			</ul>
 		</div>
 	</div>
---
+++ logout.php
@@ -0,0 +1,6 @@
+<?php
+	setcookie("user","",time()+3600);
+	setcookie("password","",time()+3600);
+	setcookie("college","",time()+3600);
+	include 'index.php';
+?>
